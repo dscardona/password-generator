493124aa1c0aaa07913b077f9a711c3a4f3bd66e
Diana Cardona <dscardona@gmail.com>
2023-05-22 17:03:48 -0400

    Fix bug, and feedback after password is copied

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "/"];
 
 
-
 function generatePw() {
     let pw1 = document.getElementById("pw1")
     let pw2 = document.getElementById("pw2")
+
+    let copiedEL1 = document.getElementById("copied-el-1")
+    copiedEL1.textContent = ""
+    let copiedEL2 = document.getElementById("copied-el-2")
+    copiedEL2.textContent = ""
+
     if (pw1.textContent.length >= 15) {
         pw1.textContent = ""
         pw2.textContent = ""
@@ -23,14 +28,17 @@ function generatePw() {
 }
 
 function copyPw1() {
-    let pw1 = document.getElementById("pw1")
     let pw1Value = pw1.textContent
     navigator.clipboard.writeText(pw1Value)
+
+    let copiedEL1 = document.getElementById("copied-el-1")
+    copiedEL1.textContent = "copied!"
 }
 
-function copyPw() {
-    let pw2 = document.getElementById("pw2")
+function copyPw2() {
     let pw2Value = pw2.textContent
     navigator.clipboard.writeText(pw2Value)
-}
 
+    let copiedEL2 = document.getElementById("copied-el-2")
+    copiedEL2.textContent = "copied!"
+}
